66e8bbe33d914963d4171564f3dd7a0294a2e95b
jphardware <jphardware2015.rj@gmail.com>
2024-01-17 17:32:05 -0300

    segundo trabalho

---
+++ partida-rankeadas.js
@@ -0,0 +1,55 @@
+
+imortal(101)
+
+function mainVitorias(){
+    ferro ()
+    bronze()
+    prata()
+    ouro()
+    diamante()
+    lendario()
+    imortal()
+
+}
+function ferro(vitoria){
+    let vitorias = vitoria
+    let rank = "ferro"
+    console.log("O heroi tem saldo de "+ vitorias + "vitorias está no nivel de " +rank )
+    return vitorias
+}
+function bronze(vitoria){
+    let vitorias = vitoria
+    let rank = "bronze"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
+function prata(vitoria){
+    let vitorias = vitoria
+    let rank = "prata"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
+function ouro(vitoria){
+    let vitorias = vitoria
+    let rank = "ouro"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
+function diamante(vitoria){
+    let vitorias = vitoria
+    let rank = "diamante"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
+function lendario(vitoria){
+    let vitorias = vitoria
+    let rank = "lendario"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
+function imortal(vitoria){
+    let vitorias = vitoria
+    let rank = "imortal"
+    console.log("O heroi tem saldo de "+ vitorias + " vitorias está no nivel de " +rank )
+    return vitorias
+}
